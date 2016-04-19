815d109b484daf34ff3969e24fbd934d676a48f5
bibo <Glen.Langer@T-Systems.com>
2016-04-19 12:58:40 +0200

    Add Language files

---
+++ languages/en/modules.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Contao Open Source CMS,Copyright (c) 2005-2016 Leo Feyer
+ *
+ * Language file for enhanced error handler
+ *
+ * @copyright  Glen Langer 2016 <http://contao.ninja>
+ * @author     Glen Langer (BugBuster)
+ * @license    LGPL
+ * @see	       https://github.com/BugBuster1701/errorlog_enhanced
+ */
+
+ $GLOBALS['TL_LANG']['MOD']['errorlog_enhanced']['0']          = 'Enhanced error.log';
+ $GLOBALS['TL_LANG']['MOD']['errorlog_enhanced']['1']          = 'Contao error.log with complete relative file paths.';
